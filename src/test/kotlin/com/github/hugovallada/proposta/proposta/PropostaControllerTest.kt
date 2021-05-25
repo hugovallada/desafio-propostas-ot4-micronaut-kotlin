f89cf4a36e9535b64ea2ec3ab5d3e14e86b95283
hugovallada <valladahugo@gmail.com>
2021-05-25 15:17:28 -0300

    Cria teste para status 422 quando o documento já existir no banco

--- src/test/kotlin/com/github/hugovallada/proposta/proposta/PropostaControllerTest.kt
+++ src/test/kotlin/com/github/hugovallada/proposta/proposta/PropostaControllerTest.kt
@@ -8,8 +8,10 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -27,19 +29,31 @@ internal class PropostaControllerTest{
     lateinit var propostaRequest: NovaPropostaRequest
 
     @field:Inject
+    lateinit var repository: PropostaRepository
+
+    @field:Inject
     lateinit var enderecoClient: EnderecoClient
+
+    lateinit var proposta: Proposta
 
     val REGEX_RETURN = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
 
     @BeforeEach
     internal fun setUp() {
         val endereco = Endereco(logradouro = "Rua Tibiriça", uf = "SP", numero = "688", "Sertãozinho")
-        propostaRequest = NovaPropostaRequest("44407669539","email@email.com","Hugo","14000090" , BigDecimal(2500), "688")
+        proposta = Proposta("44407669539","email@email.com","Hugo", Endereco("Campos","SP","66","RP"), BigDecimal(2500))
+        repository.save(proposta)
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        repository.deleteAll()
     }
 
     @Test
-    internal fun propostaDeveSerElegivelCasoUmCpfValidoSejaPassado() {
+    internal fun `proposta deve ser cadastrada quando dados válidos forem passados`() {
         // Cenário
+        propostaRequest = NovaPropostaRequest("44407669538","email@email.com","Hugo","14000090" , BigDecimal(2500), "688")
         val request = HttpRequest.POST("/propostas", propostaRequest)
         val enderecoRequest = EnderecoRequest(logradouro = "Rua Tibiriça", uf = "SP",localidade =  "Sertãozinho")
 
@@ -53,6 +67,21 @@ internal class PropostaControllerTest{
         assertEquals(HttpStatus.CREATED, response.status)
         assertTrue(response.headers.contains("Location"))
         assertTrue(response.header("Location")!!.matches("/propostas/$REGEX_RETURN".toRegex()))
+    }
+
+
+    @Test
+    internal fun `um status 422 deve ser retornado quando um documento já cadastrado for passado`() {
+        propostaRequest = NovaPropostaRequest("44407669539","email@email.com","Hugo","14000090" , BigDecimal(2500), "688")
+
+        //Cenário
+        val request = HttpRequest.POST("/propostas", propostaRequest)
+
+        assertThrows(HttpClientResponseException::class.java) {
+            val response = client.toBlocking().exchange(request, Any::class.java)
+        }.run {
+            assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, status)
+        }
 
 
 
