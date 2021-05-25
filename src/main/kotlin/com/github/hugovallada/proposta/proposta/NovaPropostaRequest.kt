f065646e0848cde092577d6881b1fb5ab0418f57
hugovallada <valladahugo@gmail.com>
2021-05-24 21:03:45 -0300

    Adiciona as bean validations ao propostaRequest

--- src/main/kotlin/com/github/hugovallada/proposta/proposta/NovaPropostaRequest.kt
+++ src/main/kotlin/com/github/hugovallada/proposta/proposta/NovaPropostaRequest.kt
@@ -2,12 +2,27 @@ package com.github.hugovallada.proposta.proposta
 
 import io.micronaut.core.annotation.Introspected
 import java.math.BigDecimal
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
 
 @Introspected
 data class NovaPropostaRequest(
+    @field:NotBlank
     val documento: String,
+    @field:Email @field:NotBlank
     val email: String,
+    @field:NotBlank
     val nome: String,
+    @field:NotBlank
     val cep: String,
+    @field:NotNull @field:Positive
     val salario: BigDecimal
-)
+){
+    fun toModel(endereco: Endereco) : Proposta {
+        return Proposta(documento, email, nome, endereco, salario)
+    }
+}
+
+
