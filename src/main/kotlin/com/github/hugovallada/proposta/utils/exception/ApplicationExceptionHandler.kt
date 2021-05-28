4c1242b8779be08e74f88222828c01f129905dea
hugovallada <valladahugo@gmail.com>
2021-05-28 08:25:09 -0300

    Transforma a classe de exception handler em um tratador genérico e limita o log

--- src/main/kotlin/com/github/hugovallada/proposta/utils/exception/ApplicationExceptionHandler.kt
+++ src/main/kotlin/com/github/hugovallada/proposta/utils/exception/ApplicationExceptionHandler.kt
@@ -5,10 +5,13 @@ import io.micronaut.context.annotation.Requirements
 import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Produces
 import io.micronaut.http.server.exceptions.ExceptionHandler
 import io.micronaut.http.server.exceptions.response.ErrorResponseProcessor
 import io.micronaut.validation.exceptions.ConstraintExceptionHandler
+import java.lang.RuntimeException
+import java.rmi.server.ServerCloneException
 import javax.inject.Singleton
 import javax.validation.ConstraintViolationException
 
@@ -17,8 +20,19 @@ import javax.validation.ConstraintViolationException
 @Replaces(ConstraintExceptionHandler::class) // assim consegue substituir o tratador de exceções padrão do Micronaut
 class ApplicationExceptionHandler:ExceptionHandler<Exception, HttpResponse<*>> {
     override fun handle(request: HttpRequest<*>, exception: Exception): HttpResponse<*> {
-        val listaDeMensagens = exception.message?.split(",") ?: return HttpResponse.badRequest(null)
 
+        when(exception){
+            is ConstraintViolationException -> {
+                val listaDeMensagens = exception.message?.split(",") ?: return HttpResponse.badRequest(null)
+                val body = handleConstraintNotValidException(listaDeMensagens)
+                return HttpResponse.badRequest(ApiErrorResponse(body))
+            }
+        }
+
+        return HttpResponse.serverError(null)
+    }
+
+    private fun handleConstraintNotValidException(listaDeMensagens: List<String>): MutableList<ApiError> {
         val body = mutableListOf<ApiError>()
 
         listaDeMensagens.forEach { lista ->
@@ -30,9 +44,9 @@ class ApplicationExceptionHandler:ExceptionHandler<Exception, HttpResponse<*>> {
                 )
             )
         }
-
-        return HttpResponse.badRequest(body)
+        return body
     }
+
 
 }
 
